ed2f3bab3a1a9029503324badccc57d4abc079b3
Hector Paulino <hector.x.paulino.-nd@disney.com>
2026-04-10 16:27:08 -0600

    apply format

--- accessing-claude-with-the-api/multi-turn-conversation.py
+++ accessing-claude-with-the-api/multi-turn-conversation.py
@@ -6,22 +6,26 @@ load_dotenv()
 client = Anthropic()
 model = "claude-sonnet-4-0"
 
+
 # helper functions
 def add_user_message(messages, text):
     user_message = {"role": "user", "content": text}
     messages.append(user_message)
 
+
 def add_assistant_message(messages, text):
     assistant_message = {"role": "assistant", "content": text}
     messages.append(assistant_message)
 
-def chat (messages):
+
+def chat(messages):
     message = client.messages.create(
         model=model,
         max_tokens=1000,
         messages=messages,
     )
     return message.context[0].text
+
 
 # conversation
 # Start with an empty message list
