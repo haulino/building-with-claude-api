28f384c52bac678a21cc7c5c8f7c535b8a10ce14
Hector Paulino <efrancisco.paulino@outlook.com>
2026-04-16 22:11:16 -0600

    test: expand unit tests (#11)
    * chore: trigger ci
    
    * test: add assistant test
    
    * chore: add dict return

--- tests/test_multi_turn_conversation.py
+++ tests/test_multi_turn_conversation.py
@@ -1,9 +1,17 @@
-from multi_turn_conversation import add_user_message
+from multi_turn_conversation import add_user_message, add_assistant_message
 
 
 def test_add_user_message():
     messages = []
     user_input = "Is tomato a fruit?"
     result = add_user_message(messages, user_input)
+
+    assert result in messages
+
+
+def test_add_assistant_message():
+    messages = []
+    input = "This would tipically be a text response from the api"
+    result = add_assistant_message(messages, input)
 
     assert result in messages
